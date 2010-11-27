2e31ae8e452337c8b08f690d95a497e805a7c248
Alex Margarit <alex.margarit@gmail.com>
2010-11-27 15:13:16 -0500

    Added simple layered sprites.

---
+++ src/a2x_pack_layersprite.c
@@ -0,0 +1,78 @@
+/*
+    Copyright 2010 Alex Margarit
+
+    This file is part of a2x-framework.
+
+    a2x-framework is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    a2x-framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with a2x-framework.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "a2x_pack_layersprite.p.h"
+#include "a2x_pack_layersprite.v.h"
+
+typedef struct Layer Layer;
+
+struct LayerSprite {
+    List* layers;
+};
+
+struct Layer {
+    Sprite* sprite;
+    BlitType_t blit;
+    uint8_t r, g, b, a;
+};
+
+LayerSprite* a_layersprite_make(void)
+{
+    LayerSprite* const ls = malloc(sizeof(LayerSprite));
+
+    ls->layers = a_list_set();
+
+    return ls;
+}
+
+void a_layersprite_free(LayerSprite* const ls)
+{
+    a_list_freeContent(ls->layers);
+
+    free(ls);
+}
+
+void a_layersprite_add(LayerSprite* const ls, Sprite* const sprite, const BlitType_t blit, const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a)
+{
+    Layer* const l = malloc(sizeof(Layer));
+
+    l->sprite = sprite;
+    l->blit = blit;
+    l->r = r;
+    l->g = g;
+    l->b = b;
+    l->a = a;
+
+    a_list_addLast(ls->layers, l);
+}
+
+void a_layersprite_blit(LayerSprite* const ls, const int x, const int y)
+{
+    List* const layers = ls->layers;
+
+    while(a_list_iterate(layers)) {
+        Layer* const l = a_list_current(layers);
+
+        a_blit_setType(l->blit);
+        a_blit_setRGB(l->r, l->g, l->b);
+        a_blit_setAlpha(l->a);
+
+        a_blit(l->sprite, x, y);
+    }
+}
---
+++ src/a2x_pack_layersprite.p.h
@@ -0,0 +1,35 @@
+/*
+    Copyright 2010 Alex Margarit
+
+    This file is part of a2x-framework.
+
+    a2x-framework is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    a2x-framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with a2x-framework.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef A2X_PACK_LAYERSPRITE_PH
+#define A2X_PACK_LAYERSPRITE_PH
+
+typedef struct LayerSprite LayerSprite;
+
+#include "a2x_pack_blit.p.h"
+#include "a2x_pack_list.p.h"
+#include "a2x_pack_sprite.p.h"
+
+extern LayerSprite* a_layersprite_make(void);
+extern void a_layersprite_free(LayerSprite* const ls);
+
+extern void a_layersprite_add(LayerSprite* const ls, Sprite* const sprite, const BlitType_t blit, const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a);
+extern void a_layersprite_blit(LayerSprite* const ls, const int x, const int y);
+
+#endif // A2X_PACK_LAYERSPRITE_PH
