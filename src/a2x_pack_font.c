d8585bddb11c42f46d851763a324d426a19861be
Alex Margarit <alex@alxm.org>
2016-01-18 16:37:53 -0800

    More compact font characters table; add the missing ` character.

--- src/a2x_pack_font.c
+++ src/a2x_pack_font.c
@@ -29,16 +29,12 @@ typedef struct Font {
     int maxWidth;
 } Font;
 
-#define CHARS_NUM 93
-static const char chars[CHARS_NUM] = {
-    'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g',
-    'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n',
-    'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u',
-    'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z',
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    '_', '-', '+', '=', '*', '/', '\\', '&', '$', '@', '!', '?', '\'', '"',
-    '(', ')', '[', ']', '{', '}', '.', ',', '~', ':', ';', '%', '^', '#', '<', '>', '|'
-};
+static const char chars[] =
+    "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
+    "0123456789"
+    "_-+=*/\\&$@!?'\"()[]{}.,~:;%^#<>|`";
+
+#define CHARS_NUM (sizeof(chars) / sizeof(char) - 1)
 
 static List* fontsList;
 static Font** fonts;
