16ddabbdd6c208605599b6c9903b5c7870d04333
Alex Margarit <alex@alxm.org>
2016-07-09 12:04:40 -0700

    makeall.sh shows build results for all platforms.

--- bin/make/a2x/cleanall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-make -f Makefile clean
-make -f Makefile.caanoo clean
-make -f Makefile.gp2x clean
-make -f Makefile.pandora clean
-make -f Makefile.wiz clean
--- bin/make/a2x/makeall.sh
+++ bin/make/a2x/makeall.sh
@@ -1,7 +1,29 @@
 #!/bin/sh
 
-make -f Makefile
-make -f Makefile.caanoo
-make -f Makefile.gp2x
-make -f Makefile.pandora
-make -f Makefile.wiz
+output=""
+make_arg=$1
+
+color_text ()
+{
+    output="$output\n\033[$1;1m$2\033[0m"
+}
+
+do_make ()
+{
+    make -f $2 $make_arg
+
+    if [ $? -eq 0 ];
+    then
+        color_text 32 "[ Success ] $1"
+    else
+        color_text 31 "[ Failure ] $1"
+    fi
+}
+
+do_make "Desktop Linux" Makefile
+do_make "Caanoo" Makefile.caanoo
+do_make "GP2X" Makefile.gp2x
+do_make "Pandora" Makefile.pandora
+do_make "Wiz" Makefile.wiz
+
+echo $output
