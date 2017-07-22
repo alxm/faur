40e884b942b31278a3b3a1a2b59902fb8dcee349
Alex Margarit <alex@alxm.org>
2017-07-22 12:01:26 -0700

    Unsigned int versions of a_random_getInt, a_random_getInRange, and a_random_chance.

--- src/a2x_pack_random.p.h
+++ src/a2x_pack_random.p.h
@@ -31,6 +31,10 @@ extern unsigned a_random_getSeed(void);
 extern void a_random_setSeed(unsigned Seed);
 
 extern int a_random_getInt(int Max);
+extern unsigned a_random_getIntu(unsigned Max);
+
 extern int a_random_getInRange(int Min, int Max);
+extern unsigned a_random_getInRangeu(unsigned Min, unsigned Max);
 
 extern bool a_random_chance(int Something, int OutOf);
+extern bool a_random_chanceu(unsigned Something, unsigned OutOf);
