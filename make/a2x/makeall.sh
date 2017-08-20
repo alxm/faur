8fbae829ee398e81ee3efca6aceb4f27eeae8ae5
Alex Margarit <alex@alxm.org>
2017-08-19 22:39:55 -0700

    Emscripten makefiles.

--- make/a2x/makeall.sh
+++ make/a2x/makeall.sh
@@ -8,16 +8,26 @@ color_text ()
     output="$output\n\033[$1;1m$2\033[0m"
 }
 
-do_make ()
+record_result ()
 {
-    make -j -f $2 $make_arg
-
-    if [ $? -eq 0 ];
+    if [ $2 -eq 0 ];
     then
         color_text 32 "[ Success ] $1"
     else
         color_text 31 "[ Failure ] $1"
     fi
+}
+
+do_make ()
+{
+    make -j -f $2 $make_arg
+    record_result "$1" $?
+}
+
+do_emmake ()
+{
+    emmake make -j -f $2 $make_arg
+    record_result "$1" $?
 }
 
 do_make "Linux native" Makefile
@@ -29,5 +39,6 @@ do_make "Caanoo" Makefile.caanoo
 do_make "GP2X" Makefile.gp2x
 do_make "Pandora" Makefile.pandora
 do_make "Wiz" Makefile.wiz
+do_emmake "Emscripten" Makefile.emscripten
 
 echo $output
