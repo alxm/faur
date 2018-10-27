c1c69f5e17175dd2bce2cb7cba7e37b0efce5cae
Alex Margarit <alex@alxm.org>
2018-10-27 15:53:18 -0700

    Print Pandora debug messages before calling system scripts, and related refactoring.

--- src/a2x_pack_platform_pandora.c
+++ src/a2x_pack_platform_pandora.c
@@ -1,5 +1,5 @@
 /*
-    Copyright 2016, 2017 Alex Margarit
+    Copyright 2016-2018 Alex Margarit
 
     This file is part of a2x-framework.
 
@@ -26,10 +26,7 @@
 #include "a2x_pack_out.v.h"
 #include "a2x_pack_str.v.h"
 
-static struct {
-    char* nub0Mode;
-    char* nub1Mode;
-} g_pandora;
+static char* g_nubModes[2];
 
 static void pandora_setNubModes(const char* Nub0, const char* Nub1)
 {
@@ -41,16 +38,16 @@ static void pandora_setNubModes(const char* Nub0, const char* Nub1)
                      Nub1);
 
     if(r < 0 || r >= (int)sizeof(cmd)) {
-        a_out__error("pandora_setNubModes snprintf failed");
+        a_out__error("pandora_setNubModes: snprintf failed");
         return;
     }
+
+    a_out__message("Set nub modes to %s, %s", Nub0, Nub1);
 
     if(system(cmd) < 0) {
         a_out__error("op_nubchange.sh failed");
         return;
     }
-
-    a_out__message("Set nub modes to %s, %s", Nub0, Nub1);
 }
 
 static void pandora_setScreenFilter(const char* Value)
@@ -62,36 +59,33 @@ static void pandora_setScreenFilter(const char* Value)
                      Value);
 
     if(r < 0 || r >= (int)sizeof(cmd)) {
-        a_out__error("pandora_setScreenFilter snprintf failed");
+        a_out__error("pandora_setScreenFilter: snprintf failed");
         return;
     }
+
+    a_out__message("Set screen filter to %s", Value);
 
     if(system(cmd) < 0) {
         a_out__error("op_videofir.sh failed");
         return;
     }
-
-    a_out__message("Set screen filter to %s", Value);
 }
 
 void a_platform_pandora__init(void)
 {
-    g_pandora.nub0Mode = NULL;
-    g_pandora.nub1Mode = NULL;
+    for(int i = 0; i < 2; i++) {
+        char path[32];
+        snprintf(path, sizeof(path), "/proc/pandora/nub%d/mode", i);
 
-    AFile* nub0 = a_file_new("/proc/pandora/nub0/mode", "r");
-    AFile* nub1 = a_file_new("/proc/pandora/nub1/mode", "r");
+        AFile* nub = a_file_new(path, "r");
 
-    if(nub0 && nub1 && a_file_lineRead(nub0) && a_file_lineRead(nub1)) {
-        g_pandora.nub0Mode = a_str_dup(a_file_lineBufferGet(nub0));
-        a_out__message("Current nub0Mode is '%s'", g_pandora.nub0Mode);
+        if(nub && a_file_lineRead(nub)) {
+            g_nubModes[i] = a_str_dup(a_file_lineBufferGet(nub));
+            a_out__message("Current nub%d mode is '%s'", i, g_nubModes[i]);
+        }
 
-        g_pandora.nub1Mode = a_str_dup(a_file_lineBufferGet(nub1));
-        a_out__message("Current nub1Mode is '%s'", g_pandora.nub1Mode);
+        a_file_free(nub);
     }
-
-    a_file_free(nub0);
-    a_file_free(nub1);
 
     // Gamepad mode
     pandora_setNubModes("absolute", "absolute");
@@ -102,13 +96,14 @@ void a_platform_pandora__init(void)
 
 void a_platform_pandora__uninit(void)
 {
-    if(g_pandora.nub0Mode && g_pandora.nub1Mode) {
-        pandora_setNubModes(g_pandora.nub0Mode, g_pandora.nub1Mode);
+    pandora_setScreenFilter("default");
+
+    if(g_nubModes[0] && g_nubModes[1]) {
+        pandora_setNubModes(g_nubModes[0], g_nubModes[1]);
     }
 
-    free(g_pandora.nub0Mode);
-    free(g_pandora.nub1Mode);
-
-    pandora_setScreenFilter("default");
+    for(int i = 0; i < 2; i++) {
+        free(g_nubModes[i]);
+    }
 }
 #endif // A_BUILD_SYSTEM_PANDORA
