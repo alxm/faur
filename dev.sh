cfa17602284d8ac2a3a8ab01ce33a07a5528373a
Alex Margarit <alex@alxm.org>
2018-10-28 01:33:48 -0700

    Makefile.pandora pnd target copies the source assets dir to the pnd packaging dir.

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # . dev.sh
-geany -i src/* make/a2x/* make/platforms/* make/project/* bin/a2x_* bin/utils/* dev.sh README.markdown todo &
+geany -i src/* make/a2x/* make/global/* make/platforms/* make/project/* bin/a2x_* bin/utils/* dev.sh README.markdown todo &
 cd make/a2x
