54f694f45f82cd296152866616ba7c05e71b8618
Alex Margarit <alex@alxm.org>
2018-11-27 18:57:54 -0800

    Move AColMap to its own module.

---
+++ src/a2x_pack_grid.c
@@ -0,0 +1,184 @@
+/*
+    Copyright 2010, 2011, 2015-2018 Alex Margarit
+
+    This file is part of a2x-framework.
+
+    a2x-framework is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    a2x-framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with a2x-framework.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "a2x_pack_grid.v.h"
+
+#include "a2x_pack_listit.v.h"
+#include "a2x_pack_math.v.h"
+#include "a2x_pack_mem.v.h"
+
+struct AColMap {
+    int w, h; // width and height of map, in submaps
+    int bitShift; // right-shift object coords by this to get submap index
+    AList*** submaps; // AList*[h][w] of AColObjects
+    AList** submapsData; // AList*[h * w] of AColObjects
+};
+
+struct AColObject {
+    const AColMap* colmap;
+    int x, y; // coords on the Colmap
+    AList* nodes; // AListNodes from submaps this object is in
+    void* context; // the game object that owns this AColObject
+};
+
+static inline int nextpow(int X)
+{
+    int power = 0;
+
+    while((1 << power) < X) {
+        power++;
+    }
+
+    return power;
+}
+
+AColMap* a_colmap_new(int Width, int Height, int MaxObjectDim)
+{
+    AColMap* m = a_mem_malloc(sizeof(AColMap));
+
+    m->bitShift = nextpow(MaxObjectDim);
+    m->w = 1 << a_math_max(0, nextpow(Width) - m->bitShift);
+    m->h = 1 << a_math_max(0, nextpow(Height) - m->bitShift);
+
+    m->submaps = a_mem_malloc((unsigned)m->h * sizeof(AList**));
+    m->submapsData = a_mem_malloc(
+                        (unsigned)m->h * (unsigned)m->w * sizeof(AList*));
+
+    for(int i = m->h; i--; ) {
+        m->submaps[i] = m->submapsData + i * m->w;
+
+        for(int j = m->w; j--; ) {
+            m->submaps[i][j] = a_list_new();
+        }
+    }
+
+    return m;
+}
+
+void a_colmap_free(AColMap* Map)
+{
+    if(Map == NULL) {
+        return;
+    }
+
+    for(int i = Map->h; i--; ) {
+        for(int j = Map->w; j--; ) {
+            // In case ColMap was freed before the ColObjects
+            A_LIST_ITERATE(Map->submaps[i][j], AColObject*, o) {
+                a_list_free(o->nodes);
+                o->nodes = NULL;
+            }
+
+            a_list_free(Map->submaps[i][j]);
+        }
+    }
+
+    free(Map->submaps);
+    free(Map->submapsData);
+    free(Map);
+}
+
+AColObject* a_colobject_new(const AColMap* Map, void* Context)
+{
+    AColObject* o = a_mem_malloc(sizeof(AColObject));
+
+    o->colmap = Map;
+    o->nodes = a_list_new();
+    o->context = Context;
+
+    return o;
+}
+
+void a_colobject_free(AColObject* Object)
+{
+    if(Object == NULL) {
+        return;
+    }
+
+    // Remove object from any lists it is in
+    a_list_freeEx(Object->nodes, (AFree*)a_list_removeNode);
+
+    free(Object);
+}
+
+void a_colobject_coordsSet(AColObject* Object, int X, int Y)
+{
+    const AColMap* m = Object->colmap;
+    AList* pt_nodes = Object->nodes;
+    AList*** submaps = m->submaps;
+
+    // remove point from all the submaps it was previously in
+    a_list_clearEx(pt_nodes, (AFree*)a_list_removeNode);
+
+    // set new coords
+    Object->x = X;
+    Object->y = Y;
+
+    // center submap coords
+    const int submap_x = Object->x >> m->bitShift;
+    const int submap_y = Object->y >> m->bitShift;
+
+    // offset inside submap
+    const int submapDim = 1 << m->bitShift;
+    int offset_x = Object->x & (submapDim - 1);
+    int offset_y = Object->y & (submapDim - 1);
+
+    // submaps perimeter
+    int start_x, end_x;
+    int start_y, end_y;
+
+    if(offset_x < submapDim / 2) {
+        start_x = a_math_clamp(submap_x - 1, 0, m->w - 1);
+        end_x = a_math_clamp(submap_x, 0, m->w - 1);
+    } else {
+        start_x = a_math_clamp(submap_x, 0, m->w - 1);
+        end_x = a_math_clamp(submap_x + 1, 0, m->w - 1);
+    }
+
+    if(offset_y < submapDim / 2) {
+        start_y = a_math_clamp(submap_y - 1, 0, m->h - 1);
+        end_y = a_math_clamp(submap_y, 0, m->h - 1);
+    } else {
+        start_y = a_math_clamp(submap_y, 0, m->h - 1);
+        end_y = a_math_clamp(submap_y + 1, 0, m->h - 1);
+    }
+
+    // add object to every submap in its surrounding perimeter
+    for(int i = start_y; i <= end_y; i++) {
+        for(int j = start_x; j <= end_x; j++) {
+            // add point to the submap, save node to point's nodes list
+            a_list_addFirst(pt_nodes, a_list_addFirst(submaps[i][j], Object));
+        }
+    }
+}
+
+void* a__colobject_contextGet(const AColObject* Object)
+{
+    return Object->context;
+}
+
+AList* a__colobject_nearbyListGet(const AColObject* Object)
+{
+    const AColMap* map = Object->colmap;
+
+    int submap_x = a_math_clamp(Object->x >> map->bitShift, 0, map->w - 1);
+    int submap_y = a_math_clamp(Object->y >> map->bitShift, 0, map->h - 1);
+
+    return map->submaps[submap_y][submap_x];
+}
---
+++ src/a2x_pack_grid.p.h
@@ -0,0 +1,47 @@
+/*
+    Copyright 2010, 2011, 2016-2018 Alex Margarit
+
+    This file is part of a2x-framework.
+
+    a2x-framework is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    a2x-framework is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with a2x-framework.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "a2x_system_includes.h"
+
+typedef struct AColMap AColMap;
+typedef struct AColObject AColObject;
+
+#include "a2x_pack_fix.p.h"
+#include "a2x_pack_list.p.h"
+
+extern AColMap* a_colmap_new(int Width, int Height, int MaxObjectDim);
+extern void a_colmap_free(AColMap* Map);
+
+extern AColObject* a_colobject_new(const AColMap* Map, void* Context);
+extern void a_colobject_free(AColObject* Object);
+
+extern void a_colobject_coordsSet(AColObject* Object, int X, int Y);
+
+extern void* a__colobject_contextGet(const AColObject* Object);
+extern AList* a__colobject_nearbyListGet(const AColObject* Object);
+
+#define A_COL_ITERATE(ColObject, ContextPtrType, ContextVarName)           \
+    for(const AColObject* a__co = ColObject; a__co; a__co = NULL)          \
+        A_LIST_FILTER(a__colobject_nearbyListGet(a__co),                   \
+                      const AColObject*, a__o,                             \
+                      a__o != a__co)                                       \
+        for(ContextPtrType ContextVarName = a__colobject_contextGet(a__o); \
+            a__o != NULL; a__o = NULL)
