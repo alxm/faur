b19dee6a07365adb7d8e6386d80449285220f460
Alex Margarit <alex@alxm.org>
2019-06-09 10:04:04 -0700

    Option to build without sound support.

--- src/a2x_pack_platform_sdl_sound.c
+++ src/a2x_pack_platform_sdl_sound.c
@@ -18,7 +18,7 @@
 
 #include "a2x_pack_platform_sdl_sound.v.h"
 
-#if A_CONFIG_LIB_SDL
+#if A_CONFIG_SOUND_ENABLED && A_CONFIG_LIB_SDL
 #if A_CONFIG_LIB_SDL == 1
     #include <SDL/SDL.h>
     #include <SDL/SDL_mixer.h>
@@ -265,4 +265,4 @@ int a_platform_api__soundSampleChannelGet(void)
 
     return g_currentSampleChannel++ % g_numSampleChannelsReserved;
 }
-#endif // A_CONFIG_LIB_SDL
+#endif // A_CONFIG_SOUND_ENABLED && A_CONFIG_LIB_SDL
