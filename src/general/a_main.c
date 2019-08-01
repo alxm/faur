a3be9dec09ea16d9dedf028b3cde74db770646f5
Alex Margarit <alex@alxm.org>
2019-08-01 16:04:12 -0700

    Fix calling the wrong free in handleFatal.

--- src/general/a_main.c
+++ src/general/a_main.c
@@ -131,7 +131,7 @@ __attribute__((noreturn)) static void handleFatal(void)
             a_out__error(functionNames[i]);
         }
 
-        a_mem_free(functionNames);
+        free(functionNames);
     #endif
 
     a_console_showSet(true);
