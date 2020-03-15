743299c2bac96d8385997ef2b54620193b60c373
Alex Margarit <alex@alxm.org>
2020-03-14 17:08:58 -0700

    Sprite sheet config file can set the default frame size for all subsequent sprites.

--- src/graphics/f_spritesheet.c
+++ src/graphics/f_spritesheet.c
@@ -30,17 +30,31 @@ FSpriteSheet* f_spritesheet_new(const char* Path)
         num += f_list_sizeGet(f_block_blocksGet(b));
     }
 
-    FHash* t = f_hash_newStr(num, true);
+    FHash* t = f_hash_newStr(f_math_minu(num, 256), true);
 
     F_LIST_ITERATE(blocks, const FBlock*, b) {
+        char path[256];
         const char* name = f_block_lineGetString(b, 0);
-        const char* path = f_str__fmt512("%s/%s", rootDirs, name);
+
+        if(!f_str_fmt(path, sizeof(path), false, "%s/%s", rootDirs, name)) {
+            F__FATAL("f_spritesheet_new(%s): f_str_fmt failed", Path);
+        }
+
         FSprite* sheet = f_sprite_newFromPng(path, 0, 0, -1, -1);
+        FVecInt fixedDim = {0, 0};
 
         F_LIST_ITERATE(f_block_blocksGet(b), const FBlock*, b) {
             const char* id = f_block_lineGetString(b, 0);
+
+            if(id[0] == '*') {
+                fixedDim = f_block_lineGetCoords(b, 1);
+
+                continue;
+            }
+
             FVecInt coords = f_block_lineGetCoords(b, 1);
-            FVecInt dim = f_block_lineGetCoords(b, 2);
+            FVecInt dim = fixedDim.x > 0
+                            ? fixedDim : f_block_lineGetCoords(b, 2);
 
             FSprite* sprite = f_sprite_newFromSprite(
                                 sheet, coords.x, coords.y, dim.x, dim.y);
