bd5cba2f097c31ea2de5698ccdf4e554ae393373
Alex Margarit <alex@alxm.org>
2020-10-27 14:22:54 -0700

    Move all SDK path declarations to their own sdk.mk file.

--- make/global/defs.mk
+++ make/global/defs.mk
@@ -47,20 +47,8 @@ F_DIR_ROOT ?= $(realpath $(F_DIR_ROOT_FROM_MAKE))
 F_FAUR_FILE_SDK_MK := $(F_FAUR_DIR_CONFIG)/sdk.mk
 F_FAUR_FILE_GEANY_TAGS := $(HOME)/.config/geany/tags/faur.c.tags
 
+#
+# Include the user SDK paths, then conditionally-set the default paths
+#
 -include $(F_FAUR_FILE_SDK_MK)
-
-F_SDK_ARDUINO_ROOT ?= /opt/arduino
-F_SDK_ARDUINO_MAKEFILE ?= /opt/Arduino-Makefile
-
-F_SDK_CAANOO_ROOT ?= /opt/gph_sdk
-F_SDK_CAANOO_TOOLCHAIN ?= /opt/gph_sdk/tools/gcc-4.2.4-glibc-2.7-eabi
-
-F_SDK_EMSCRIPTEN_ROOT ?= /opt/emsdk
-
-F_SDK_MINGW32_ROOT ?= /usr
-F_SDK_MINGW64_ROOT ?= /usr
-
-F_SDK_OPEN2X_TOOLCHAIN ?= /opt/open2x/gcc-4.1.1-glibc-2.3.6
-
-F_SDK_PANDORA_TOOLCHAIN ?= /opt/pandora/arm-2013.11
-F_SDK_PANDORA_UTILS ?= /opt/pandora/sdk_utils
+include $(FAUR_PATH)/make/global/sdk.mk
