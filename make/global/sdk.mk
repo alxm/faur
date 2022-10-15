c17588a727869711c312d3879432f07e52e1bf87
Alex Margarit <alex@alxm.org>
2022-10-15 16:12:16 -0700

    Remove MinGW 32bit target and update related config.

--- make/global/sdk.mk
+++ make/global/sdk.mk
@@ -10,8 +10,7 @@ F_SDK_CAANOO_TOOLCHAIN ?= /opt/gph_sdk/tools/gcc-4.2.4-glibc-2.7-eabi
 
 F_SDK_EMSCRIPTEN_ROOT ?= /opt/emsdk
 
-F_SDK_MINGW32_ROOT ?= /usr
-F_SDK_MINGW64_ROOT ?= /usr
+F_SDK_MINGW64_ROOT ?= /usr/x86_64-w64-mingw32
 
 F_SDK_OPEN2X_TOOLCHAIN ?= /opt/open2x/gcc-4.1.1-glibc-2.3.6
 
